386b57369b9577a82a351df23b875100074c6f3b
Martin Makarsky <martinmakarsky@gmail.com>
2020-08-26 12:56:03 +0200

    Add X-KC-SOURCE tracking header

--- src/data/home.js
+++ src/data/home.js
@@ -1,8 +1,17 @@
 const KenticoContent = require('@kentico/kontent-delivery');
+const version = require('../../package.json').version;
+
 const HOME_ITEM_CODENAME = "home"
+const sourceTrackingHeaderName = 'X-KC-SOURCE'
 
 const deliveryClient = new KenticoContent.DeliveryClient({
-    projectId: 'e7bde8f4-0bd9-0013-c0f6-0b46c4a768c4'
+    projectId: 'e7bde8f4-0bd9-0013-c0f6-0b46c4a768c4',
+    globalHeaders: (_queryConfig) => [
+      {
+        header: sourceTrackingHeaderName,
+        value: `eleventastic-kontent;${version}`,
+      },
+    ],
   });
 
 
